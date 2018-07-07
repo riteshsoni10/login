75e6cc89fdae7cb998124eae8851e947eb4a2f24
riteshsoni10 <32491007+riteshsoni10@users.noreply.github.com>
2018-07-08 00:55:11 +0530

    Add files via upload

---
+++ includes/connect.php
@@ -0,0 +1,19 @@
+<?php
+
+$server="localhost";
+        $database="login";
+        $username="root";
+        $password="Soni@1995";
+        
+ if($connect=mysql_connect($server,$username,$password,$database))
+ {
+     
+ }
+ else
+     {
+     echo "error";
+ }
+ 
+     
+         
+?>
